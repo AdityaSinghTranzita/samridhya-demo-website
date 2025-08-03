29c24a46e39a3e91e0ce2c05572c91bf0bfefb03
AdityaSinghTranzita <adityasingh@tranzita.com>
2025-08-03 15:25:03 +0530

    added pixel code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -72,7 +72,7 @@ export default function Document() {
         />
         
         {/* ==========================================
-           FACEBOOK META PIXEL CODE - TRACKING ID: 1421958515748064
+           FACEBOOK META PIXEL CODE - TRACKING ID: 1014691167229448
            ========================================== */}
         <script
           dangerouslySetInnerHTML={{
@@ -85,7 +85,7 @@ export default function Document() {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1421958515748064');
+              fbq('init', '1014691167229448');
               fbq('track', 'PageView');
             `,
           }}
@@ -96,7 +96,7 @@ export default function Document() {
             height="1" 
             width="1" 
             style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1421958515748064&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1014691167229448&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
